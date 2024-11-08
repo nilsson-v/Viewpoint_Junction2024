a900cd09547e262a5c2b1ecd1ddbb52e7e7afa74
elmernik <113885272+elmernik@users.noreply.github.com>
2024-11-08 20:50:15 +0200

    add functionality for generating polis discussions based on news

--- prompts.py
+++ prompts.py
@@ -19,4 +19,20 @@ CHOOSE_POLITICAL_NEWS_PROMPT = """You are an expert on deciding which news are b
     #####
     Please strictly stick to the expected format in your responses. Let's begin... """
 
-CREATE_POLIS_DISCUSSION_PROMPT = ""
+CREATE_POLIS_DISCUSSION_PROMPT = """You are now an expert at facilitating civilized political discourse. 
+    Your task is to create some initial statements for a thought-provoking discussion based on a recent news article.
+    Please in your response include a brief summary of the topic as a headline as well as the initial statements separeted by |.
+    Always answer in the same language as the news article. For example if the news is in Finnish then answer in finnish etc.
+    #####
+    Example:
+    #####
+    Input:
+    The finnish economy is doing badly because of agricultural strikes
+    #####
+    Output:
+    #####
+    Headline: Impact of agricultural strikes on the economy
+    
+    Initial statements: Agricultural strikes are a fair way to protect workers rights | Agricultural strikes do more harm than good | The impact of strikes is often overstated
+    ######
+    Please strictly stick to the expected format in your responses. Let's begin... """
