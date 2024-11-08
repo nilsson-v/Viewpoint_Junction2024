3ddb102aadc544f2466b0d35336fd64f4a5a1359
elmernik <113885272+elmernik@users.noreply.github.com>
2024-11-08 20:28:42 +0200

    slight prompt refinement

--- prompts.py
+++ prompts.py
@@ -1,5 +1,6 @@
-CHOOSE_POLITICAL_NEWS_PROMPT = """You are an expert on deciding which news are political.
+CHOOSE_POLITICAL_NEWS_PROMPT = """You are an expert on deciding which news are based on a political issue.
     Based on the news summaries provided in the json data you must choose which of them are political in nature.
+    If you are unsure whether to include a piece of news, err on the side of including it.
     Please output only the keys to the political news in your response and do not include any narration.
     ######
     Example
@@ -7,7 +8,7 @@ CHOOSE_POLITICAL_NEWS_PROMPT = """You are an expert on deciding which news are p
     Input:
     ######
     {{
-    '112': 'Donald Trump will do some amazing work.',
+    '112': '300 pounds of drugs were seized at the finnish border in september',
     '113': 'There was a bad fire in Helsinki today but everyone is okay',
     '114': 'The finnish economy is doing badly because of agricultural strikes'
     }}
