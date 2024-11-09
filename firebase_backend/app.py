b18a59259e4b883e252e7cab29fafed6420bd72c
elmernik <113885272+elmernik@users.noreply.github.com>
2024-11-09 12:15:19 +0200

    add similarity search

--- firebase_backend/app.py
+++ firebase_backend/app.py
@@ -3,6 +3,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1.vector import Vector
+from google.cloud.firestore_v1.base_vector_query import DistanceMeasure
+
 
 app = Flask(__name__)
 
@@ -45,6 +47,29 @@ def add_article():
 
     return jsonify({"response": "success"})
 
+
+@app.route("/similarity_search_article", methods=["POST"])
+def similarity_search_article():
+    data = request.json
+    subtopic = data.get("topic", "")
+    embedded_subtopic = embed_model.embed_query(subtopic)
+
+    collection = db.collection("news")
+
+    # Requires a single-field vector index
+    vector_query = collection.find_nearest(
+        vector_field="embedded_subtopic",
+        query_vector=Vector(embedded_subtopic),
+        distance_measure=DistanceMeasure.COSINE,
+        limit=5,
+        distance_result_field="vector_distance"
+    )
+
+    docs = vector_query.stream()
+
+    response_data = [f"{doc.id}), Distance: {doc.get('vector_distance')}" for doc in docs]
+
+    return jsonify({"response": response_data})
 
 
 if __name__ == "__main__":
