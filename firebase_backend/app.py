da548c96a52ba02b7c0f321ca46f28db45fe1a94
elmernik <113885272+elmernik@users.noreply.github.com>
2024-11-09 11:45:20 +0200

    add article test

--- firebase_backend/app.py
+++ firebase_backend/app.py
@@ -1,20 +1,51 @@
 from flask import Flask, request, jsonify
 from langchain_huggingface import HuggingFaceEmbeddings
-import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1.vector import Vector
 
 app = Flask(__name__)
+
+cred = credentials.Certificate("./junction-2024-firebase-adminsdk-nkyl9-f1b4744457.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 embed_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
 
 
-@app.route("/generate", methods=["POST"])
-def generate_text():
+@app.route("/embed_text", methods=["POST"])
+def embed_text():
     data = request.json
     user_input = data.get("text", "")
     # Embed the user input
     response = embed_model.embed_query(user_input)
     
-    return jsonify({"response": response})
+    return jsonify({"embedded_text": response})
+
+
+@app.route("/add_article", methods=["POST"])
+def add_article():
+    data = request.json
+
+    article = data.get("article", "")
+    subtopic = data.get("topic", "")
+    statements = data.get("statements", "")
+
+    embedded_subtopic = embed_model.embed_query(subtopic)
+
+    news_doc = { 
+        "topic" : subtopic,
+        "article": article,
+        "statements": statements,
+        "embedded_subtopic": Vector(embedded_subtopic)
+        }
+
+    db.collection("news").document("1").set(news_doc)
+
+    return jsonify({"response": "success"})
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- test_flask_endpoint.py
+++ test_flask_endpoint.py
@@ -1,10 +1,16 @@
 import requests
 
-url = "http://127.0.0.1:8080/generate"
-data = {"text": "Test input for embedding"}
+url1 = "http://127.0.0.1:8080/embed_text"
+url2 = "http://127.0.0.1:8080/add_article"
+data1 = {"text": "Test input for embedding"}
+data2 = {
+    "topic": "Sisäministerin tehtävän jakautuminen ja poliittinen jatkuvuus",
+    "statements": "Ministerin henkilökohtaiset syyt eivät saa vaikuttaa hallituksen toimintaan | Tyttären sairastuminen oli hyvä syy väliaikaiselle ministerinvaihdolle | Liikenne- ja viestintäministeri Lulu Ranne pystyi hoitamaan sisäministerin tehtävää hyvin väliaikaisesti",
+    "article": "KOTIMAA         YLE  PERJANTAI Mari Rantanen palaa sis\u00e4ministeriksi Perussuomalaisten kansanedustaja Mari Rantanen palaa t\u00e4n\u00e4\u00e4n sis\u00e4ministerin teht\u00e4v\u00e4\u00e4n. Rantanen j\u00e4i teht\u00e4v\u00e4st\u00e4 v\u00e4liaikaisesti sivuun elokuun lopulla tytt\u00e4rens\u00e4 sairastumisen vuoksi. Silloin h\u00e4n arvioi, ett\u00e4 voisi palata ministeriksi vasta vuodenvaihteen paikkeilla. Vakavasti sairaana ollut tyt\u00e4r on kuitenkin toipunut ennakoitua nopeammin. Perussuomalaisten Lulu Ranne on t\u00e4ll\u00e4 v\u00e4lin hoitanut sis\u00e4ministerin teht\u00e4v\u00e4\u00e4 liikenne- ja viestint\u00e4ministerin pestins\u00e4 ohella."
+    }
 
 try:
-    response = requests.post(url, json=data)
+    response = requests.post(url2, json=data2)
     response.raise_for_status()  # Raises an HTTPError if the status is 4xx, 5xx
     print(response.json())
 except requests.exceptions.RequestException as e:
