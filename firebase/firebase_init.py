edec86f581c74d49e45306d736f11ef64dc5f7be
Daniel Michaeli <danielmichaeli@Daniels-Laptop.local>
2024-11-09 10:03:10 +0200

    change key name

--- firebase/firebase_init.py
+++ firebase/firebase_init.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 # Use the service account key file from the firebase folder
-cred = credentials.Certificate("firebase/junction-2024-firebase-adminsdk-nkyl9-fe3b8a0f78.json")
+cred = credentials.Certificate("firebase/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 print("Firebase initialized successfully!")
